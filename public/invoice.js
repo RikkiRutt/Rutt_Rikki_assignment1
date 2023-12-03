237964c6a02e290f29a34cb20913fcb9128a141f
RikkiRutt <rikkir@hawaii.edu>
2023-12-02 19:07:57 -1000

    a1

--- public/invoice.js
+++ public/invoice.js
@@ -2,7 +2,6 @@
 // Get the URL
 let params = (new URL(document.location)).searchParams;
 
-
 // On load, if there is no 'valid' key, redirect the user back to the Home page
 window.onload = function() {
     if (!params.has('valid')) {
@@ -83,66 +82,3 @@ document.querySelector('#total_display').innerHTML += `
         <td><b>$${total.toFixed(2)}</td>
     </tr>
 `;
-
-
-
-
-/*calculate subtotal
-let subtotal = extended_price1 + extended_price2 + extended_price3 + extended_price4 + extended_price5;
-
-calcuate sales tax
-let taxRate = 0.0575;  // 5.75%
-let taxAmount = subtotal * taxRate;
-
-//calculate shipping
-let shippingCharge = 0
-if (subtotal <= 50)
-{
-    shippingCharge = 2
-} //lest then$50 is $2
-else if (subtotal <= 100) 
-{
-    shippingCharge = 5
-} //$50 to $99.99 is $5
-else
-{
-    shippingCharge= subtotal * 0.05 //5% of subtotal
-} //more then $100 is 5% of subtotal
-//calculate total
-let total = subtotal + taxAmount + shippingCharge;
-
-//populate the table rows using dom manipulation
-let table = document.getElementById('invoiceTable');
-
-let row = invoiceTable.insertRow(); //create new row for each item
-row.insertCell(0).innerHTML = `${item1}`; 
-row.insertCell(1).innerHTML = `${quantity1}`;
-row.insertCell(2).innerHTML = `$` + `${price1}`;
-row.insertCell(3).innerHTML = (`$` + `${extended_price1}`);
-row = invoiceTable.insertRow(); //create new row for each item
-row.insertCell(0).innerHTML = `${item2}`; 
-row.insertCell(1).innerHTML = `${quantity2}`;
-row.insertCell(2).innerHTML = `$` + `${price2}`;
-row.insertCell(3).innerHTML = (`$` + `${extended_price2}`);
-row = invoiceTable.insertRow(); //create new row for each item
-row.insertCell(0).innerHTML = `${item3}`; 
-row.insertCell(1).innerHTML = `${quantity3}`;
-row.insertCell(2).innerHTML = `$` + `${price3}`;
-row.insertCell(3).innerHTML = (`$` + `${extended_price3}`);
-row = invoiceTable.insertRow(); //create new row for each item
-row.insertCell(0).innerHTML = `${item4}`; 
-row.insertCell(1).innerHTML = `${quantity4}`;
-row.insertCell(2).innerHTML = `$` + `${price4}`;
-row.insertCell(3).innerHTML = (`$` + `${extended_price4}`);
-row = invoiceTable.insertRow(); //create new row for each item
-row.insertCell(0).innerHTML = `${item5}`; 
-row.insertCell(1).innerHTML = `${quantity5}`;
-row.insertCell(2).innerHTML = `$` + `${price5}`;
-row.insertCell(3).innerHTML = (`$` + `${extended_price5}`);
-
-//set subt, tax and total cells
-document.getElementById(`subtotal_cell`).innerHTML = `$` + subtotal.toFixed(2);
-document.getElementById(`tax_cell`).innerHTML = `$` + taxAmount.toFixed(2);
-document.getElementById(`shipping_cell`).innerHTML = `$` + shippingCharge.toFixed(2);
-document.getElementById(`total_cell`).innerHTML = `$` + total.toFixed(2);
-*/
